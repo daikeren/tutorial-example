4bd82f1e41f4f1d52a83c04b17890c1114daecce
starwilly <willystar@gmail.com>
2014-11-21 23:31:52 +0800

    hello world

--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# trips/views.py
 
-# Create your views here.
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse("Hello World!")
