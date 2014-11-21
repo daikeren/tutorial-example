6d4734f43b3de8540dea731aad807b97ad5e0352
starwilly <willystar@gmail.com>
2014-11-21 23:37:04 +0800

    hello world with template

--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -1,6 +1,10 @@
 # trips/views.py
 
-from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    return render(request,
+                  'hello_world.html',
+                  {'current_time': datetime.now()})
